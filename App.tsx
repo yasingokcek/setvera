076f98534cc015a929da542b67d584e8d45993c0
yasingokcek <yasin.gokcek@aimusic.com.tr>
2026-02-25 22:09:46 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SetveraChat from './components/SetveraChat';
+
+const App: React.FC = () => {
+    return (
+          <div className="min-h-screen">
+                <SetveraChat />
+          </div>div>
+        );
+};
+
+export default App;</div>
